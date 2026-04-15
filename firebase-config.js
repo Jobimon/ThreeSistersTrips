316327b33beafdb7e1e1b80d6758a1ca0b93ebac
Jobimon <jobi.augustine@gmail.com>
2026-04-15 07:25:06 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDA3vgzzLHyg2d0Xv3XnKU2gSjDyGBvWr0",
+  authDomain: "threesisterseuropetrip.firebaseapp.com",
+  projectId: "threesisterseuropetrip",
+  storageBucket: "threesisterseuropetrip.firebasestorage.app",
+  messagingSenderId: "421699331524",
+  appId: "1:421699331524:web:f8abd3e3aa24c6fc293091"
+};
