68d551f79b91400ace21dab8134037e0783e48fb
Jobimon <jobi.augustine@gmail.com>
2026-04-19 23:11:20 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIza....",
   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abc123"
 };
 
-// Firestore document that stores the shared checklist state
 window.CHECKLIST_DOC_PATH = "tripChecklists/three-sisters-europe-2026";
