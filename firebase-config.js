31f6eb06d9b648bcb408cc75d8d6e01cde87295d
Jobimon <jobi.augustine@gmail.com>
2026-04-19 23:29:03 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,9 +2,9 @@ window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDA3vgzzLHyg2d0Xv3XnKU2gSjDyGBvWr0",
   authDomain: "threesisterseuropetrip.firebaseapp.com",
   projectId: "threesisterseuropetrip",
-  storageBucket: "threesisterseuropetrip.appspot.com", // fix here
+  storageBucket: "threesisterseuropetrip.appspot.com", 
   messagingSenderId: "421699331524",
-  appId: "REPLACE_WITH_REAL_APP_ID" // fix here
+  appId: "1:421699331524:web:f8abd3e3aa24c6fc293091"
 };
 
 window.CHECKLIST_DOC_PATH = "tripChecklists/three-sisters-europe-2026";
